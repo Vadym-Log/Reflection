8e270a2ae9ab80f4cf222b4645fc4be124eecbe7
Vadym-Log <94236146+Vadym-Log@users.noreply.github.com>
2021-11-14 17:47:28 +0200

    Add files via upload

---
+++ Task_03/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Task_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 27);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(223, 411);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(241, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(547, 411);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyReflector";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Task_03/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Task_03
+{
+    public partial class Form1 : Form
+    {
+        Assembly assembly = null;
+        Type[] types;
+        FieldInfo[] fields;
+        Type[] interfaces;
+        MemberInfo[] members;
+        ConstructorInfo[] constructors;
+        PropertyInfo[] properties;
+        MethodInfo[] methods;
+        ParameterInfo[] parameters;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                treeView1.Nodes.Clear();
+                textBox1.Clear();
+                string filestr = openFileDialog1.FileName;
+                assembly = Assembly.LoadFile(filestr);
+                textBox1.Text += "СБОРКА    " + filestr + "  -  УСПЕШНО ЗАГРУЖЕНА" + Environment.NewLine + Environment.NewLine;
+
+                types = assembly.GetTypes();
+                foreach (Type t in types)
+                {
+                    treeView1.Nodes.Add(t.ToString());
+                }
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+		{
+			string nameclass = treeView1.SelectedNode.Text;
+			if (e.Node.Parent == null)
+			{
+				textBox1.Clear();
+				for (int i = 0; i < types.Length; ++i)
+					if (nameclass == types[i].FullName)
+					{
+						if (types[i].BaseType != null)
+							textBox1.Text += "     Для данного класса базовым является " + types[i].BaseType + Environment.NewLine;
+						if (types[i].IsAbstract)
+							textBox1.Text += "     Данный класс является абстрактным" + Environment.NewLine;
+
+						fields = types[i].GetFields();
+						interfaces = types[i].GetInterfaces();
+						members = types[i].GetMembers();
+						constructors = types[i].GetConstructors();
+						properties = types[i].GetProperties();
+						methods = types[i].GetMethods();
+					}
+				textBox1.Text += Environment.NewLine;
+				textBox1.Text += "       Список всех интерфейсов класса: " + Environment.NewLine;
+				foreach (Type t in interfaces)
+					textBox1.Text += t.Name + Environment.NewLine;
+				textBox1.Text += Environment.NewLine;
+
+				textBox1.Text += "       Список всех полей класса: " + Environment.NewLine;
+				foreach (FieldInfo f in fields)
+					textBox1.Text += f.Name + Environment.NewLine;
+				textBox1.Text += Environment.NewLine;
+
+				textBox1.Text += "       Список всех свойств класса: " + Environment.NewLine;
+				foreach (PropertyInfo p in properties)
+					textBox1.Text += p.Name + Environment.NewLine;
+				textBox1.Text += Environment.NewLine;
+
+				textBox1.Text += "       Список всех членов класса: " + Environment.NewLine;
+				foreach (MemberInfo m in members)
+					textBox1.Text += m.Name + Environment.NewLine;
+
+				if (treeView1.SelectedNode.Nodes.Count == 0)
+				{
+					foreach (ConstructorInfo c in constructors)
+						treeView1.SelectedNode.Nodes.Add("Конструктор " + c.Name);
+					foreach (MethodInfo m in methods)
+						treeView1.SelectedNode.Nodes.Add("Метод " + m.Name);
+				}
+			}
+
+			for (int i = 0; i < constructors.Length; ++i)
+			{
+				if (nameclass == ("Конструктор " + constructors[i].Name))
+				{
+					textBox1.Clear();
+					textBox1.Text += "         Параметры конструктора" + Environment.NewLine;
+					parameters = constructors[i].GetParameters();
+					foreach (ParameterInfo p in parameters)
+					{
+						textBox1.Text += "Имя: " + p.Name + Environment.NewLine;
+						textBox1.Text += "Позиция: " + p.Position + Environment.NewLine;
+						textBox1.Text += "Тип: " + p.ParameterType + Environment.NewLine + Environment.NewLine;
+					}
+					textBox1.Text += Environment.NewLine;
+					textBox1.Text += "        Тело конструктора" + Environment.NewLine;
+					MethodBody methbody = constructors[i].GetMethodBody();
+					textBox1.Text += methbody.ToString();
+				}
+			}
+			for (int i = 0; i < methods.Length; ++i)
+			{
+				if (nameclass == ("Метод " + methods[i].Name))
+				{
+					textBox1.Clear();
+					textBox1.Text += "         Параметры метода" + Environment.NewLine;
+					parameters = methods[i].GetParameters();
+					foreach (ParameterInfo p in parameters)
+					{
+						textBox1.Text += "Имя: " + p.Name + Environment.NewLine;
+						textBox1.Text += "Позиция: " + p.Position + Environment.NewLine;
+						textBox1.Text += "Тип: " + p.ParameterType + Environment.NewLine + Environment.NewLine;
+					}
+					textBox1.Text += "Тип возвращаемого значения: " + methods[i].ReturnType;
+					textBox1.Text += Environment.NewLine + Environment.NewLine;
+					textBox1.Text += "        Тело метода" + Environment.NewLine;
+					MethodBody methbody = methods[i].GetMethodBody();
+					textBox1.Text += methbody.ToString();
+				}
+			}
+		}
+	}
+}
