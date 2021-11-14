7996eaceab2400e3113d15b2613d7e8b1423ead1
Vadym-Log <94236146+Vadym-Log@users.noreply.github.com>
2021-11-14 17:39:15 +0200

    Add files via upload

---
+++ Task_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using Task_01;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.Write("Введите температуру в градусах Цельсия: ");
+            float temperature = Convert.ToInt16(Console.ReadLine());
+            
+            TemperatureConverter temperatureConverter = new TemperatureConverter();
+
+            Type type = temperatureConverter.GetType();
+
+            MethodInfo mi = type.GetMethod("CelsiusToFahrenheit", BindingFlags.Static | BindingFlags.Public);
+
+            float result = (float)mi.Invoke(temperatureConverter, new object[] { temperature });
+
+            Console.WriteLine("Температура в градусах Фаренгейта = {0}", result);
+            Console.ReadKey();            
+        }
+    }
+}
