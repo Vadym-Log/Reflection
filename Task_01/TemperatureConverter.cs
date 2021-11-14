5968c93b994d8382169d7530dba0707f88a883ba
Vadym-Log <94236146+Vadym-Log@users.noreply.github.com>
2021-11-14 17:44:23 +0200

    Add files via upload

---
+++ Task_01/TemperatureConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_01
+{
+    public class TemperatureConverter
+    {
+        public static float CelsiusToFahrenheit(float t)
+        {
+            return t * 1.8f + 32f;
+        }
+    }
+}
